7dbe244b7537984c28a32793487720b2ad275462
Josue <josue@zipcodersmbp10.lan>
2024-07-06 16:22:39 -0400

    diceandbinsdone

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Bins {
 
+    int low;
+    int high;
+
+    public Bins(int low, int high) {
+        this.low = low;
+        this.high = high;
+    }
+
+    static HashMap<Integer, Integer> binMap = new HashMap<Integer, Integer>();
+
+    //creates Bins and puts them into a hashmap with (Key: low ++ until high )(Value: occurrences from dice rolled)
+    public void createBins() {
+        for (int i = low; i <= high; i++) {
+            int temp = low++;
+            binMap.put(temp,0);
+        }
+        System.out.println(binMap);
+    }
+
+    public void incrementBin(int key){
+        binMap.get(key);
+        binMap.put(key, binMap.get(key) + 1);
+    }
+
+    public int getBin(int key){
+        return binMap.get(key);
+    }
 }
--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,19 @@
 public class Dice {
 
+    int numOfDice;
 
+    public Dice (int numOfDice){
+        this.numOfDice = numOfDice;
+    }
+
+    public int tossAndSum (){
+        int sum = 0;
+        for (int i = 0; i < numOfDice; i++){
+            int diceRoll = (int)(Math.random() * 6 + 1);
+            System.out.println("one dice roll: " + diceRoll);
+            sum += diceRoll;
+        }
+        System.out.println(sum);
+        return sum;
+    }
 }
